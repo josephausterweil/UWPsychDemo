8e04ba686a1b76fb9b4b5458fa229e371028fcae
josephausterweil <joseph.austerweil@gmail.com>
2020-03-04 21:25:35 -0600

    added linear model analyses

--- scripts/play.R
+++ scripts/play.R
@@ -8,3 +8,13 @@ describe(parenthood)
 
 ggplot(parenthood, aes(x=baby.sleep,y=dan.grump)) + geom_point(size=4)
 ggplot(parenthood, aes(x=baby.sleep,y=dan.sleep)) + geom_point(size=2, shape=2)
+
+baby_sleep_grump = lm(dan.grump ~ baby.sleep, data=parenthood)
+
+summary(baby_sleep_grump)
+
+dani_grump_x_baby = lm(dan.grump ~ dan.sleep*baby.sleep, data=parenthood)
+
+summary(dani_grump_x_baby)
+
+anova(baby_sleep_grump, dani_grump_x_baby)
